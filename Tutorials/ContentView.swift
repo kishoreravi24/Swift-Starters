0cf86c34b38a08e44f8559eeab2d7508e19ca9f2
kishore Jr <36214175+kishoreravi24@users.noreply.github.com>
2021-04-16 09:05:58 +0530

    Initial Commit

---
+++ Tutorials/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  Tutorials
+//
+//  Created by Kishore Jr on 16/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isHearted = false
+    @State private var numberOfTimes = 0
+    var body: some View {
+        VStack {
+            Button(action: {
+                self.isHearted.toggle()
+                if isHearted {
+                    self.numberOfTimes += 1
+                }
+            }) {
+                if isHearted {
+                    VStack {
+                        Image("highlands")
+                            .resizable()
+                            .cornerRadius(30)
+                            .padding()
+                        Image(systemName: "heart.fill")
+                            .padding()
+                        Text("number of times heart clicker: \(numberOfTimes)")
+                    }
+                } else {
+                    VStack {
+                        Text("Wanna to look mountains, click love icon :)")
+                            .foregroundColor(.black)
+                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                            .italic()
+                            .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                        Image(systemName: "heart")
+                            .padding()
+                            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    }
+
+                }
+            }
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tutorials/TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialsApp.swift
+//  Tutorials
+//
+//  Created by Kishore Jr on 16/04/21.
+//
+
+import SwiftUI
+
+@main
+struct TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
